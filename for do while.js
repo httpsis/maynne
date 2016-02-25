4052ae7e4268f44f3b4c40a2b4681171faee23ad
httpsis <httpsis@naver.com>
2016-02-26 01:08:44 +0900

    for do while 연습
    으아 루프

---
+++ for do while.js
@@ -0,0 +1,17 @@
+// Write your code below!
+
+for (var i = 0; i <5; i++){
+    console.log(i);
+}
+
+var u = true;
+while (u){
+    console.log(u);
+    u = false;
+}
+
+var y = 0
+do { y++
+console.log(y);
+}
+while(y<5)
